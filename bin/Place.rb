6c770546f63731c5217845a25f6338934a6142c8
Amy with dogs <concon.wolf@gmail.com>
2017-11-03 18:46:09 +0900

    Player, Token, BaseObject

--- bin/Place.rb
+++ bin/Place.rb
@@ -1,10 +1,17 @@
 require 'json'
+
 class Place
-  attr_reader :sym , :name , :adjs
+  attr_reader :to_sym , :name , :adjs
+
   def initialize(key , value)
-    @sym = key.to_sym
+    @to_sym = key.to_sym
     @name = value["name"]
     @adjs = value["adjs"].reject{|val| val == ""}.uniq
+    @holding = {}
+  end
+  def parachute(place)
+    warn "Warning:It is not recommended that instance of Place do 'parachute' cause it should be parahuted."
+    super(place)
   end        
   def adj?(place)
     @adjs.include?(place)
@@ -26,22 +33,18 @@ class Places
     def reconnect
       @places.each_pair do |key , place|
         place.adjs.each do |pl|
-          @places[pl.to_sym]&.add_adj(key.to_s)
+          @places[pl.to_sym].add_adj(key.to_s)
         end
       end
     end
   end
 end
-JSON.parse(File.open("Places.json","r").read).each_pair do |key,value|
+
+PlaceDef = "def/Place.json"
+JSON.parse(File.open(PlaceDef,"r").read).each_pair do |key,value|
   eval <<-EOS
     #{key.capitalize} = Place.new('#{key}',#{value})
-    #{key.capitalize}.freeze
     Places[:#{key.to_sym}] = #{key.capitalize}
   EOS
 end
 Places.reconnect
-
-
-p Atlanta.adjs
-p Places[:sankt_peterburg].name
-p Places.places.each_value{|val| p [val.name,val.adjs]}
---
+++ bin/Token.rb
@@ -0,0 +1,42 @@
+require 'json'
+
+class Token
+  attr_reader :to_sym , :name , :subtype
+  def initialize(key , value = {})
+    @to_sym = key.to_sym
+    @name = value["name"]
+    @num = 0
+    @subtype = \
+      case value["subtype"] 
+      when String then eval "#{value['subtype'].capitalize}.new"
+      when Array 
+        buf = {}
+        value["subtype"].each{|sub| buf[sub.to_sym] = eval "#{sub.capitalize}.new"}
+        buf
+      else nil
+    end
+    @subtype = @subtype.subtype while @subtype.is_a?(Token)
+  end
+  def [](type)
+    @subtype[type.to_sym] || @hold[type.to_sym]
+  end
+  def add(num = 1)
+    @num += num
+  end
+  def reduce(num = 1)
+    @num -= num
+  end
+end
+
+TokenDef = "def/Token.json"
+JSON.parse(File.open(TokenDef,"r").read).each_pair do |key , value|
+  eval <<-EOS
+    class #{key.capitalize} < Token
+      def initialize(key = '#{key}',value = #{value})
+        super(key,value)
+      end
+    end
+  EOS
+end
+
+
