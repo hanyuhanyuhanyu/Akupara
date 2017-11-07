dcedf33e58abc0bd118e832b980fcb540782b5eb
hanyu <concon.wolf@gmail.com>
2017-11-07 18:15:06 +0900

    Token fix

--- bin/process/methods/hoge.rb
+++ bin/process/methods/hoge.rb
@@ -1,11 +1,9 @@
 def token_load
   puts "this method will distribute enomerous number of token on you board."
-  Tokens[:disease] = Disease.new(init:true)
-  Tokens[:cure] = Cure.new(init:true)
-  Tokens[:base] = Base.new(init:true)
+  Tokens.setup(:disease,:cure,:base)
 #  p Tokens
 #  p Tokens[:disease]
-  p Tokens[:base]
+  p Tokens
 end
 def confirm_role
   puts "this method will distribute roles to every player."
