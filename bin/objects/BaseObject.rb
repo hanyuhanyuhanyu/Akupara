9139586d0fbbe14e85de8e006faba887bde4b4b9
hanyuhanyuhanyu <concon.wolf@gmail.com>
2017-11-14 00:43:38 +0900

    defaultboard create

--- bin/objects/BaseObject.rb
+++ bin/objects/BaseObject.rb
@@ -27,3 +27,5 @@ class BaseObject
 end
 
 
+
+
--- bin/objects/Place.rb
+++ bin/objects/Place.rb
@@ -1,15 +1,14 @@
 require 'json'
-
 class Place
   attr_reader :name , :adjs
 
   def initialize(key , value)
     @to_sym = key.to_sym
     @name = value["name"]
-    @adjs = value["adjs"].reject{|val| val == ""}.uniq.map(&:to_sym)
+    @adjs = value["adjs"]&.reject{|val| val == ""}.uniq.map(&:to_sym)
     @holding = {}
     @direction = {}
-    %w|right left up down down_right up_right down_left up_left|.each do |dir|
+    DefaultBoard.def_dirs.each do |dir|
       @direction[dir.to_sym] = value[dir].to_sym if value[dir]
     end
   end
@@ -25,7 +24,7 @@ class Place
   end
 end
 class PlaceHolder < Hash
-  def reconnect
+   def reconnect
     self.each_pair do |key , place|
       place.adjs.each do |pl|
         self[pl.to_sym].add_adj(key.to_s)
@@ -33,6 +32,43 @@ class PlaceHolder < Hash
     end
   end
 end
+class DefaultBoard < Hash
+  @default_dirs = %w|right_down right right_up down up left_down left left_up|
+  def self.def_dirs; @default_dirs;end
+	def initialize(row , col , origin = "left_up")
+    @row = row; @col = col
+  	directions = DefaultBoard.def_dirs
+  	grid = [1,0,-1]
+    grid_defs = {
+  		"left_up" => [grid , grid],
+  		"right_up" => [grid.reverse , grid],
+  		"left_down" => [grid , grid.reverse],
+  		"right_down" => [grid.reverse , grid.reverse]
+  	}
+  	grid_defs.each_pair{|key , val| grid_defs[key] = val[0].product(val[1]).reject{|item| item.all?(&:zero?)}}
+  	@grid_hash = {}
+  	directions.each_with_index{|dir , ind| @grid_hash[dir] = grid_defs[origin || "left_up"][ind]}
+        
+    all_arr = [*0...row].product([*0...col])
+    all_arr.each{|i| self["r#{i[0]}c#{i[1]}".to_sym] = 0}
+    all_arr.each do |i| 
+      buf_hash ={}
+      buf_hash["name"] = "r#{i[0]}c#{i[1]}"
+      buf_hash["adjs"] = []
+      @grid_hash.each_pair do |key , item|
+        arr = [i[0]+item[0],i[1]+item[1]]
+        p key
+        next unless self["r#{arr[0]}c#{arr[1]}".to_sym]
+        buf_hash["adjs"] << "r#{arr[0]}c#{arr[1]}" unless key.include?("_")
+        buf_hash[key] = "r#{arr[0]}c#{arr[1]}" 
+      end
+      self["r#{i[0]}c#{i[1]}".to_sym] = Place.new(buf_hash["name"] , buf_hash)
+    end
+	end
+end
+hoge =DefaultBoard.new(8,8)
+p hoge
+
 Places = PlaceHolder.new
 PlaceDef = "#{ __FILE__.split("/")[0..-2].join("/")}/def/Place.json"
 PlaceJson = JSON.parse(File.open(PlaceDef,"r").read)
