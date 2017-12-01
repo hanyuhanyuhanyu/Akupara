b6cd0c974f8f786af9f737e58b50e1ae22c3a6a4
後藤 良太 <r_goto@jse.jpsoft.jp>
2017-12-01 21:30:08 +0900

    add forgotten

---
+++ bin/def/Token.rb
@@ -0,0 +1,72 @@
+require 'json'
+
+class Token
+  attr_reader :name , :subtype , :amount
+  def initialize(key , value = {})
+    @to_sym = key.to_sym
+    @name = value["name"]
+    @num = 1
+    @ally = nil
+  end
+  def init(amo = nil)
+    case amo
+    when Hash 
+      amo.each_pair do |key,value|
+        value.times{hold(eval "#{key.to_s.capitalize}.new")}
+      end
+    when Integer
+      @num = amo
+    end
+  end
+  def add(num = 1)
+    @num += num
+  end
+  def reduce(num = 1)
+    @num -= num
+    nil if @num <= 0
+  end
+end
+
+class TokenHolder < Hash
+  def setup(*token)
+    token.flatten.each{|token| self[token] = eval "#{token.to_s.capitalize}.new(init:true)"}
+  end
+end
+Tokens = TokenHolder.new
+
+TokenDef = "#{ __FILE__.split("/")[0..-2].join("/")}/def/Token.json"
+TokenJson = JSON.parse(File.open(TokenDef,"r").read)
+TokenJson.each_value do |value|
+  next unless value["amount"].is_a?(Hash)
+  while value["amount"].keys.any?{|sub| TokenJson[sub]["subtype"]}
+    value["amount"].keys.each do |sub|
+      next unless TokenJson[sub]["subtype"]
+      [TokenJson[sub]["subtype"]].flatten.each{|grandsub| value["amount"][grandsub] ||= value["amount"][sub]}
+      value["amount"].delete sub
+    end
+  end
+end
+TokenJson.each_value do |value|
+  next unless value["subtype"]
+  value["subtype"] = [value["subtype"]].flatten
+  value["subtype"].map!{|sub| TokenJson[sub]["subtype"] || sub}.flatten! while value["subtype"].any?{|sub| TokenJson[sub]["subtype"]}
+  value["subtype"] = value["subtype"][0] if value["subtype"].length == 1
+end
+TokenJson.each_pair do |key , value|
+  eval <<-EOS
+    class #{key.capitalize} < Token
+      prepend BaseInitialize
+      @@subtype = #{value['subtype'] || []}
+      @@amount = #{value['amount'] || 0}
+      def initialize(**opt)
+        super('#{key}',#{value})
+        init(@@amount) if opt[:init]
+      end
+    end
+  EOS
+end
+TokenJson.each_pair do |key , value|
+  eval <<-EOS
+    Tokens[:#{key}] = #{key.capitalize}.new(init:true)
+  EOS
+end
---
+++ bin/def/obj_package.rb
@@ -0,0 +1,15 @@
+dir = __FILE__.split("/")[0..-2].join("/")
+require "#{dir}/BaseObject.rb"
+files = Dir.glob(dir + "/*").select{|fl| fl.split("/")[-1] =~ /^[A-Z].*\.rb$/ && fl.split("/")[-1] != "BaseObject.rb"}
+
+files.map do |fl|
+  fl.split("/")[-1].gsub(/\..+$/,"")
+end.each do |kls|
+  eval "class #{kls} \n inherit_basics\n end"
+end
+
+files.reject do |file|
+  File::ftype(file) == "directory"
+end.each do |path|
+  require path
+end
