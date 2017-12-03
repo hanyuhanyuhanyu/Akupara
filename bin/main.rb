c3c433eee00e6d3f2221bf79dcae11f653435499
hanyu <concon.wolf@gmail.com>
2017-12-03 22:39:40 +0900

    edit main.rb again

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,11 @@
+exit if ARGV.empty?
+game = ARGV.join("/")
+dir = Dir.glob("./#{game}")[0]
+dir.split("/").<<("").inject do |path,sub| 
+  setter = "#{path}/def/setting.rb"
+  require_relative setter if File.exist?(setter)
+  path += "/".+(sub)
+end
 require_relative "objects/package.rb"
 require_relative "process/sequence.rb"
 Sequences.run
