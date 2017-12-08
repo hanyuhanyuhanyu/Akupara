27efb94cabc96dab5b73e8ede28bdce4fefa453f
hanyu <concon.wolf@gmail.com>
2017-12-09 07:54:05 +0900

    repo change

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,13 @@
-exit if ARGV.empty?
+(warn "No ARGV Exception."; exit)if ARGV.empty?
 game = ARGV.join("/")
 dir = Dir.glob("./#{game}")[0]
+(warn "#{game} not found." ; exit ) if dir.nil?
 dir.split("/").<<("").inject do |path,sub| 
   setter = "#{path}/def/setting.rb"
   require_relative setter if File.exist?(setter)
   path += "/".+(sub)
 end
+Sequences = Sequence.new
 Sequences.run
 
 
