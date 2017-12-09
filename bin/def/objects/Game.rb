0ffedb29d778a78a4db8f172cd0d3c13f43c3d49
hanyuhanyuhanyu <concon.wolf@gmail.com>
2017-12-09 18:23:25 +0900

    reorganize folder structure done. Game class require run method.

---
+++ bin/def/objects/Game.rb
@@ -0,0 +1,80 @@
+require "json"
+class Definer
+  def initialize(file)
+    @file = file
+  end
+  def define
+  end
+end
+class PlaceDefiner < Definer
+  def initialize(file)
+    super
+    placejson = JSON.parse(File.open(@file,"r").read)
+    ret_places = 
+    if setting = placejson["default_board"]  
+      DefaultBoard.new(setting["row"],setting["col"],setting)
+    else
+      placejson.each_pair do |key,value|
+        eval <<-EOS
+          #{key.capitalize} = Place.new('#{key}',#{value})
+          ret_places[:#{key.to_sym}] = #{key.capitalize}
+        EOS
+      end
+    end
+    ret_places.reconnect
+    ret_places
+  end
+end
+class PlayerDefiner < Definer
+end
+class TokenDefiner < Definer
+  def initialize(file) 
+    super
+    token_json = JSON.parse(File.open(@file,"r").read)
+    token_json.each_value do |value|
+      next unless value["amount"].is_a?(Hash)
+      while value["amount"].keys.any?{|sub| token_json[sub]["subtype"]}
+        value["amount"].keys.each do |sub|
+          next unless token_json[sub]["subtype"]
+          [token_json[sub]["subtype"]].flatten.each{|grandsub| value["amount"][grandsub] ||= value["amount"][sub]}
+          value["amount"].delete sub
+        end
+      end
+    end
+    token_json.each_value do |value|
+      next unless value["subtype"]
+      value["subtype"] = [value["subtype"]].flatten
+      value["subtype"].map!{|sub| token_json[sub]["subtype"] || sub}.flatten! while value["subtype"].any?{|sub| token_json[sub]["subtype"]}
+      value["subtype"] = value["subtype"][0] if value["subtype"].length == 1
+    end
+    token_json.each_pair do |key , value|
+      eval <<-EOS
+        class #{key.capitalize} < Token
+          prepend BaseInitialize
+          @@subtype = #{value['subtype'] || []}
+          @@amount = #{value['amount'] || 0}
+          def initialize(**opt)
+            super('#{key}',#{value})
+            init(@@amount) if opt[:init]
+          end
+        end
+      EOS
+    end
+    tokens = TokenHolder.new
+    token_json.each_pair do |key , value|
+      eval <<-EOS
+        tokens[:#{key}] = #{key.capitalize}.new(init:true)
+      EOS
+    end
+    tokens
+  end
+end
+module Akupara
+  class Game
+    def self.set_def_files(**files)
+      @@places = PlaceDefiner.new files[:place]
+      @@players = PlayerDefiner.new files[:player]
+      @@tokens = TokenDefiner.new files[:token]
+    end
+  end
+end
