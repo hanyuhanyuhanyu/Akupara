d66c168fdeeeb363035561807dc16590a7354cbf
hanyu <concon.wolf@gmail.com>
2017-12-09 18:23:21 +0900

    reorganize folder structure done. Game class require run method.

--- bin/def/objects/pack.rb
+++ bin/def/objects/pack.rb
@@ -1,4 +1,4 @@
 here_path = File.expand_path("../",__FILE__)
-require here_path+"BaseObject.rb"
+require here_path+"/BaseObject.rb"
 Dir.glob(File.expand_path("../*.rb",__FILE__)).select{|f| f !~ /BaseObject.rb/ && f=~/[A-Z]/}.each{|f| require f}
-require here_path+"Game.rb"
+require here_path+"/Game.rb"
