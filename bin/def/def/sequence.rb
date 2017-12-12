43d4bf7bc9a1fa75699778b300c82d322c798bae
hanyu <concon.wolf@gmail.com>
2017-12-12 13:17:38 +0900

    arrange folder construction

--- bin/def/def/sequence.rb
+++
@@ -1,6 +0,0 @@
-require "json"
-require_relative "methods/package.rb"
-
-require_relative "../objects/package.rb"
-
-
--- bin/main.rb
+++ bin/main.rb
@@ -6,8 +6,13 @@ dir = Dir.glob("./#{game}")[0]
 (warn "#{game} not found." ; exit ) if dir.nil?
 
 dir.split("/").<<("").inject do |path,sub| 
-  setter = "#{path}/def/setting.rb"
+  setter = "#{path}/setting.rb"
   require_relative setter if File.exist?(setter)
+  def_file = path + "/def"
+  while Dir.exist?(def_file)
+    ("#{def_file}/setting.rb").tap{|pt| break unless File.exist?(pt); require_relative pt}
+    def_file += "/def"
+  end
   path += "/".+(sub)
 end
 Akupara::Game.new.run
